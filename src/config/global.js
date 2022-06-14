4005f06f977235166f428cce72395bdd4aabb697
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-06-14 16:28:55 -0500

    Ajustes banner

--- src/config/global.js
+++ src/config/global.js
@@ -15,6 +15,10 @@ export default {
         clases: ['banner-principal-decorativo-2'],
         imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
       },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.svg'),
+      },
     ],
   },
   menuPrincipal: {
@@ -34,30 +38,42 @@ export default {
         nombreRuta: 'tema1',
         icono: 'far fa-file-alt',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Preposiciones de tiempo',
         desarrolloContenidos: true,
-        subMenu: [
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
-            hash: 't_1_1',
-          },
-        ],
       },
 
       {
         nombreRuta: 'tema2',
         icono: 'far fa-file-alt',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: '<em>What do I do during the day?</em>',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.1',
+            titulo: 'Verbos comunes para expresar rutinas personales',
+            hash: 't_2_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.2',
+            titulo: 'Pasatiempos',
+            hash: 't_2_2',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.3',
+            titulo: ' Horas del día',
+            hash: 't_2_3',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         icono: 'far fa-file-alt',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: '<em>Likes and dislikes</em> / Gustos y disgustos',
         desarrolloContenidos: true,
       },
     ],
@@ -102,36 +118,69 @@ export default {
   },
   complementario: [
     {
-      tema: 'Estrategias para la segmentación de mercados',
-      referencia:
-        'Cámara de Comercio de Medellín. (2020). <em>Suftware</em> Tendencias de negocios - Mercadeo y ventas',
-      tipo: 'Video',
-      link: 'https://www.google.com/',
+      tema: 'Preposiciones de tiempo',
+      referencia: 'British Council. (s. f.). Prepositions of time. ',
+      tipo: 'Web',
+      link:
+        'https://learnenglishteens.britishcouncil.org/grammar/a1-a2-grammar/prepositions-time',
     },
     {
-      tema: 'Estrategias para la segmentación de mercados',
-      referencia:
-        'Cámara de Comercio de Medellín. (2020). <em>Suftware</em> Tendencias de negocios - Mercadeo y ventas',
+      tema: 'What do I do during the day?',
+      referencia: 'British Council. (s. f.). Daily routine. ',
+      tipo: 'Web',
+      link:
+        'https://learnenglishteens.britishcouncil.org/vocabulary/a1-a2-vocabulary/daily-routine',
+    },
+    {
+      tema: 'Likes and dislikes / Gustos y disgustos',
+      referencia: 'Tres por Mil. (2017). LIKE Y DISLIKE [Video]. YouTube. ',
       tipo: 'Video',
-      descarga: '/downloads/prueba.pdf',
+      link: 'https://www.youtube.com/watch?v=OFr15L2N5FE',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: '<em>Demonym</em> / Gentilicio:',
+      significado:
+        'es un adjetivo que expresa el origen geográfico o racial de la persona.',
     },
     {
-      termino: '<em>Término</em>',
-      significado: 'Definición',
+      termino: '<em>Hobby</em> / Pasatiempo:',
+      significado:
+        'es una actividad que la persona lleva a cabo para mantenerse entretenida durante un periodo de tiempo libre.',
+    },
+    {
+      termino: '<em>Introducing myself</em> / Presentación personal:',
+      significado:
+        'información que se da ante una o varias personas respecto al nombre, apellido, edad, dirección, entre otros detalles que es necesario proporcionar de acuerdo con el contexto en el que se desarrolla la situación.',
+    },
+    {
+      termino: '<em>Personal pronouns</em> / Pronombres personales:',
+      significado:
+        'son palabras que sustituyen al nombre, permitiendo nombrar a los seres, objetos y situaciones sin usar un sustantivo.',
+    },
+    {
+      termino: '<em>Time prepositions</em> / Preposiciones de tiempo:',
+      significado:
+        'son expresiones que nos dan información sobre el tiempo en que sucede algo.',
     },
   ],
   referencias: [
     {
-      referencia:
-        'Zlackman, C. (2012). 100 años de moda. Sistema de Bibliotecas SENA:',
+      referencia: 'British Council. (2014). Prepositions of time.',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/primo-explore/fulldisplay?docid=sena_aleph000060086&vid=SENA&search_scope=sena_completo&tab=sena_completo&lang=es_ES&context=L',
+        'https://learnenglishteens.britishcouncil.org/grammar/a1-a2-grammar/prepositions-time',
+    },
+    {
+      referencia:
+        'González, A. y Sierra, N. (2008). Retos y posibilidades de la enseñanza del inglés basada en contenidos en la educación superior: visión de los docentes en una experiencia en Colombia. Núcleo, 20(25), p. 125-148. ',
+      link:
+        'http://ve.scielo.org/scielo.php?script=sci_arttext&pid=S0798-97842008000100007&lng=es&tlng=es',
+    },
+    {
+      referencia:
+        'Redecker, C. (2017). European framework for the digital competence of educators: DigCompEdu. Publications Office of the European Union',
+      link: 'https://publications.jrc.ec.europa.eu/repository/handle/JRC107466',
     },
   ],
   creditos: {
@@ -149,6 +198,28 @@ export default {
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
+      {
+        nombre: 'Miguel Ángel Cárdenas López',
+        cargo: 'Experto temático',
+        centro:
+          'CENIGRAF - Centro para la Industria de la Comunicación Gráfica - Regional Distrito Capital ',
+      },
+      {
+        nombre: 'Óscar Absalón Guevara',
+        cargo: 'Diseñador instruccional',
+        centro:
+          'Centro de la Industria, la Empresa y los Servicios – CIES - Regional Santander',
+      },
+      {
+        nombre: 'Ana Catalina Córdoba Sus',
+        cargo: 'Asesora metodológica',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital',
+      },
+      {
+        nombre: 'Darío González',
+        cargo: 'Corrector de estilo',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital',
+      },
     ],
     desarrolloProducto: [
       {
@@ -158,25 +229,25 @@ export default {
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Yuly Andrea Rey Quiñonez',
         cargo: 'Diseño web',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: '',
         cargo: 'Producción audiovisual',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Camilo Andres Bolaño Rey',
         cargo: 'Desarrollo front-end',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: '',
         cargo: 'Validación de diseño y contenido',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
